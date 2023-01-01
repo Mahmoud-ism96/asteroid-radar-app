6e50b697d8e36f200339040857b33c22c935873b
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-01-01 06:04:34 +0200

    Added AsteroidAdapter

---
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -0,0 +1,50 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.databinding.AsteroidItemBinding
+
+class AsteroidAdapter(private val onClickListener: OnClickListener) :
+    ListAdapter<Asteroid, AsteroidAdapter.AsteroidViewHolder>(DiffCallback) {
+    companion object DiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+    class AsteroidViewHolder(private var binding: AsteroidItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(asteroid: Asteroid) {
+            binding.asteroid = asteroid
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AsteroidViewHolder {
+        return AsteroidViewHolder(AsteroidItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
+        val asteroid = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(asteroid)
+        }
+        holder.bind(asteroid)
+    }
+    class OnClickListener(val clickListener: (asteroid: Asteroid) -> Unit) {
+        fun onClick(asteroid: Asteroid) = clickListener(asteroid)
+    }
+}
