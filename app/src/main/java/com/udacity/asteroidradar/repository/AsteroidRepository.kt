9341480b75491d98c4c48559df0b25508262e098
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-01-01 05:20:25 +0200

    Added Repository

---
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
@@ -0,0 +1,30 @@
+package com.udacity.asteroidradar.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.database.asDatabaseModel
+import com.udacity.asteroidradar.database.asDomainModel
+import com.udacity.asteroidradar.network.AsteroidApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+
+class AsteroidRepository(private val database: AsteroidDatabase) {
+
+    val asteroids: LiveData<List<Asteroid>> = Transformations.map(database.asteroidDao.getAsteroids()) {
+        it.asDomainModel()
+    }
+
+    suspend fun refreshAsteroids() {
+        withContext(Dispatchers.IO) {
+            val asteroidList = AsteroidApi.retrofitService.getProperties(Constants.API_KEY)
+            val result = parseAsteroidsJsonResult(JSONObject(asteroidList))
+            database.asteroidDao.insertAll(*result.asDatabaseModel())
+        }
+    }
+}
